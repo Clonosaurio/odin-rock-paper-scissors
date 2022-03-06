4ca4e069be3d947c017ec487d7a2538de42daa96
Clonosaurio <96928935+Clonosaurio@users.noreply.github.com>
2022-03-06 13:32:29 -0300

    commented problem solving

---
+++ rock-paper-scissors.js
@@ -0,0 +1,12 @@
+/* problem solving for Rock Paper Scissors game.
+- Rules:
+rock beats scissors, paper beats rock, scissors beat paper.
+- Planning the game:
+tree options will be displayed, Rock, Paper and Scissors.
+the "machine" will chose one too, randomly.
+after player choses, the chosen items by player and machine
+will be compared.
+Following the rules, the winner will get a point.
+The first on getting 5 points will be declared as the winner.
+The game should be able to be restarted to play again. */
+
